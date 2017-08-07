6fa234632618f0a352f71be38ea098106d7c9e18
Rajakhader <30662265+Rajakhader@users.noreply.github.com>
2017-08-07 19:34:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //Move the image
-var img = document.getElementById('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpN-7MaRtIbKXZm2cqNFu2xUgtYp5N44VwWsFvhnnFU1yxRAnn');
+var img = document.getElementById('doodle');
 img.onclick = function () {
   img.style.marginleft = '100px';  
 };
