f1335dedd127ae49d575d7e0aa20eb6a84efa0f1
Rajakhader <30662265+Rajakhader@users.noreply.github.com>
2017-08-07 19:28:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //Move the image
-var img = document.getElementById('madi');
+var img = document.getElementById('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpN-7MaRtIbKXZm2cqNFu2xUgtYp5N44VwWsFvhnnFU1yxRAnn');
 img.onclick = function () {
   img.style.marginleft = '100px';  
 };
